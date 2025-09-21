0852314b113c7c21818c3ddd5fa5347c8e26a6d4
Noah Kremer <75767608+nkremer20@users.noreply.github.com>
2025-09-21 00:16:12 -0500

    Added visibility and animation logic to dropdown button

--- src/index.js
+++ src/index.js
@@ -1,1 +1,22 @@
 import './styles.css';
+
+const dropdowns = document.querySelectorAll('.dropdown-btn');
+
+dropdowns.forEach((dropdown) => {
+    dropdown.addEventListener('click', (e) => {
+        const dropdownOptions = dropdown.nextElementSibling;
+        
+        const dropdownVisibility = dropdownOptions.checkVisibility({visibilityProperty: true});
+
+        if (dropdownVisibility) {
+            dropdownOptions.style.visibility = 'hidden';
+            dropdown.style.width = '40px'
+            dropdownOptions.classList.remove('animate');
+        } else {
+            dropdown.style.width = '100%'
+            dropdownOptions.style.visibility = 'visible';
+            dropdownOptions.classList.add('animate')     
+        }
+        
+    })
+})
